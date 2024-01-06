cb2837b49aaf0334ce84ac64eaa1d96db77b471d
nursultan <nursultanmuratjanov1@gmail.com>
2024-01-06 18:38:03 +0600

    init

---
+++ locators/elements_page_locators.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+
+class TextBoxPageLocators:
+
+
+    # from fields https://demoqa.com/text-box
+
+    FULL_NAME = (By.CSS_SELECTOR,"input[id='userName']")
